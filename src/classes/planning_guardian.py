0fe48048ddabaa607a7f777706b5e18b20bef312
Andra Veraart <averaart@hotmail.com>
2011-11-28 08:35:20 +0100

    Started new class for planning

---
+++ src/classes/planning_guardian.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 27, 2011
+
+@author: averaart
+'''
+
+class PlanGuardian(object):
+    '''
+    Collects all relevant data for an event for one guardian
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
