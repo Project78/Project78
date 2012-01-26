eb18435115faae741108f44fe97e73cf77ae6dfa
Tom Nieuwenhuijs <t.nieuwenhuys@hotmail.com>
2012-01-26 17:23:11 +0100

    created class CreatePDF

--- src/handlers/mailevent.py
+++ src/handlers/mailevent.py
@@ -1,8 +1,5 @@
 
 import os
-import sys
-
-sys.path.insert(0, 'reportlab.zip')
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
@@ -12,14 +9,7 @@ from google.appengine.api import users
 
 from models.guardian import Guardian
 from models.teacher import Teacher
-
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
-
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
+from classes.CreatePDF import CreatePDF
 
 class MailHandler(webapp.RequestHandler):
     title = 'title'
@@ -72,65 +62,10 @@ class MailHandler(webapp.RequestHandler):
                     message.subject = self.title
                     message.body = self.text
                     if not self.attach == '':
-                        pdf = self.createPDF(self.attach)
+                        pdf = CreatePDF().createPDF(self.attach)
                         if not pdf == None:
                             message.attachments = [(self.title + '.pdf', pdf)]
 #                            print pdf
                     message.Send()
                     print 'E-mail send to %s' % to_addr
-                    
-    def createPDF(self, text):
-        if not text.strip() == '':
-            buf = StringIO()
-            p = canvas.Canvas(buf, pagesize=A4)
-            p.setFont('Helvetica-Bold', 20)
-            p.drawString(50, 780, "Donald Knuth College")
-            p.line(50, 774, 540, 774)
-            
-            parts = text.split('\n')
-            p.setFont('Helvetica', 10)
-            i = 0
-            jump = 16
-            limit = 750
-            lineWidth = 110
-            
-            for part in parts:
-                if i + 60 <= limit:
-                    part = part.strip();
-                    pts = part.split(' ')
-                    s = ''
-                    for pt in pts:
-                        if len(s) + len(pt) < lineWidth:
-                            s += pt + ' '
-                        else:
-                            if i + 60 <= limit:
-                                p.drawString(50, limit - i, s.strip())
-                                s = pt + ' '
-                                i += jump
-                            else:
-                                self.nextPage(p)
-                                p.drawString(50, limit, s.strip())
-                                s = pt + ' '
-                                i = jump
-                    
-                    p.drawString(50, limit - i, s.strip())
-                    i += jump
-                else:
-                    self.nextPage(p)
-                    p.drawString(50, limit, part.strip())
-                    i = jump
-
-            p.save()
-            pdf = buf.getvalue()
-            buf.close() 
-            return pdf
-        else:
-            return None
-        
-    def nextPage(self, p):
-        p.showPage()
-        p.setFont('Helvetica-Bold', 20)
-        p.drawString(50, 780, "Donald Knuth College")
-        p.line(50, 774, 540, 774)
-        p.setFont('Helvetica', 10)
     
